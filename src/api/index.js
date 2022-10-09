a466a3aae30f16eec70826a944068653cea95fd4
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-09 17:12:35 +0000

    add basic crud operations to memories

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(URL);
+export const createPost = newPost => axios.post(URL, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${URL}/${id}`, updatedPost);
+export const deletePost = id => axios.delete(`${URL}/${id}`);
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import moment from 'moment';
+import { Card, CardActions, CardContent, Button, Typography, CardMedia } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+import useStyles from './styles';
+
+const Post = ({ post, setCurrentId }) => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: 'white' }} size="small" onClick={() => { setCurrentId(post._id) }}>
+                    <MoreHorizIcon fontSize="medium" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="secondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+            </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+            <CardContent>
+                <Typography variant="body1" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardContent className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => { }}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => { }}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Post;
